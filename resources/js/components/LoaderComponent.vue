198b3628f5b988fca01fa52b86c025b31d650a2f
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-09-30 15:16:04 +0200

    Loader done

---
+++ resources/js/components/LoaderComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container text-center">
+    <img src="https://media.tenor.com/7VN-8uaMPEkAAAAC/really-cat.gif" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
